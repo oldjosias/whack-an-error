852cd80704ba15476572a2e2f1c9f93c026a5cb1
Josias Old <josias.old@rwth-aachen.de>
2025-09-06 19:45:50 +0200

    Update Tutorial

--- static/js/i18n.js
+++ static/js/i18n.js
@@ -50,12 +50,17 @@
       'tutorial.prev': 'Zurück',
       'tutorial.next': 'Weiter',
       'tutorial.slides': [
-        '<b>Willkommen bei Fehler-Whack!</b><br><br>Dies ist ein Spiel zur Quantenfehlerkorrektur. Dein Ziel ist es, Fehler in einem Gitter von Qubits zu finden und zu korrigieren.<br><img src=\'static/tutorial_1.png\' style=\'width:80%;margin-top:12px;\'>',
-        'In jeder Runde haben einige Qubits Fehler. Benutze deine Schaufel, um Qubits zu flippen und versuche, alle Sensoren auszuschalten.<br><img src=\'static/tutorial_2.png\' style=\'width:80%;margin-top:12px;\'>',
-        'Sensoren zeigen an, ob sich Fehler in der Nähe befinden. Wenn alle Sensoren aus sind, hast du alle detektierbaren Fehler entfernt.<br><img src=\'static/tutorial_3.png\' style=\'width:80%;margin-top:12px;\'>',
-        'Aber Vorsicht! Wenn du alle Syndrome entfernst, aber ein logischer Fehler bleibt, verlierst du die Runde.<br><b>Logischer Fehler:</b> Ein Fehler, der von den Sensoren nicht erkannt wird und die kodierte Information verändert.<br><img src=\'static/tutorial_4.png\' style=\'width:80%;margin-top:12px;\'>',
-        'Im Spielplatz-Modus kannst du Fehler manuell setzen und die Auswirkungen direkt beobachten.<br><img src=\'static/tutorial_5.png\' style=\'width:80%;margin-top:12px;\'>',
-        'Versuche, alle Fehler zu beseitigen, ohne einen logischen Fehler zu verursachen. Steige durch die Level für mehr Herausforderung!<br>Viel Erfolg!<br><img src=\'static/tutorial_6.png\' style=\'width:80%;margin-top:12px;\'>'
+        '<b>Willkommen bei Whack-an-Error!</b><br><br>Dies ist ein Spiel zur Quantenfehlerkorrektur. Dein Ziel ist es, Tunnel Fehler in einem Gitter von Qubits zu finden und zu zuzuschütten(korrigieren).<br>Die Biber vom Jülicher See wollen nämlich auf die andere Seite.<br><img src=\'static/tutorial_1.png\' style=\'width:80%;margin-top:12px;\'>',
+        'In jeder Runde Graben die Biber einige Tunnel. Ein Tunnel aktiviert seine benachbarten Sensoren.<br><img src=\'static/tutorial_2.png\' style=\'width:80%;margin-top:12px;\'>',
+        'Grenzt ein Sensor an 1 oder 3 Tunnel, ist er aktiviert.<br>Grenzt ein Sensor an 0, 2 oder 4 Tunnel, ist er deaktiviert.<br><img src=\'static/tutorial_3.png\' style=\'width:80%;margin-top:12px;\'>',
+        'Im Spiel kannst du die Tunnel von der Oberfläche aber natürlich nicht sehen.<br><img src=\'static/tutorial_4.png\' style=\'width:80%;margin-top:12px;\'>',
+        'Markiere solange Felder zur Korrektur (Tunnel graben oder zuschütten).<br><img src=\'static/tutorial_5.png\' style=\'width:80%;margin-top:12px;\'>',
+        'Markiere solange Felder zur Korrektur (Tunnel graben oder zuschütten).<br><img src=\'static/tutorial_6.png\' style=\'width:80%;margin-top:12px;\'>',
+        'Bis alle Sensoren aus sind.<br><img src=\'static/tutorial_7.png\' style=\'width:80%;margin-top:12px;\'>',
+        'Es gibt mehrere Wege zum Ziel!  Nimm am Besten den kürzesten!<br><br>Eine Schleife von Tunneln wie hier ist kein Problem.<br><img src=\'static/tutorial_8.png\' style=\'width:80%;margin-top:12px;\'>',
+        'Nur wenn die Biber am Ende von der einen zur anderen Seite können, hast du verloren - ein logischer Fehler!<br><b>Logischer Fehler:</b> Eine durchgehender Tunnel, der von den Sensoren nicht erkannt wird und die Biber auf die andere Seite lässt.<br><img src=\'static/tutorial_9.png\' style=\'width:80%;margin-top:12px;\'>',
+        'Im Spielplatz-Modus kannst du Fehler manuell setzen und die Auswirkungen direkt beobachten.<br><img src=\'static/tutorial_10.png\' style=\'width:80%;margin-top:12px;\'>',
+        'Versuche, alle Fehler zu beseitigen, ohne einen logischen Fehler zu verursachen. Steige durch die Level für mehr Herausforderung!<br>Viel Erfolg!'
       ]
     },
     en: {
@@ -107,13 +112,18 @@
       'tutorial.prev': 'Back',
       'tutorial.next': 'Next',
       'tutorial.slides': [
-        "<b>Welcome to Whack-an-Error!</b><br><br>This is a game about quantum error correction. Your goal is to find and correct errors in a lattice of qubits.<br><img src='static/tutorial_1.png' style='width:80%;margin-top:12px;'>",
-        "Each round, some qubits have errors. Use your shovel to flip qubits and try to turn all sensors off.<br><img src='static/tutorial_2.png' style='width:80%;margin-top:12px;'>",
-        "Sensors indicate whether there are errors nearby. If all sensors are off, you've removed all detectable errors.<br><img src='static/tutorial_3.png' style='width:80%;margin-top:12px;'>",
-        "Beware! If you remove all syndromes but a logical error remains, you lose the round.<br><b>Logical error:</b> an error undetected by the sensors that changes the encoded information.<br><img src='static/tutorial_4.png' style='width:80%;margin-top:12px;'>",
-        "In playground mode, you can set errors manually and observe the effects directly.<br><img src='static/tutorial_5.png' style='width:80%;margin-top:12px;'>",
-        "Try to eliminate all errors without causing a logical one. Advance through levels for more challenge!<br>Good luck!<br><img src='static/tutorial_6.png' style='width:80%;margin-top:12px;'>"
-      ]
+                        '<b>Welcome to Whack-an-Error!</b><br><br>This is a game about quantum error correction. Your goal is to find and fill (correct) tunnel errors in a grid of qubits.<br>The beavers from Jülicher See want to get to the other side.<br><img src=\'static/tutorial_1.png\' style=\'width:80%;margin-top:12px;\'>',
+                        'In each round, the beavers dig some tunnels. A tunnel activates its neighboring sensors.<br><img src=\'static/tutorial_2.png\' style=\'width:80%;margin-top:12px;\'>',
+                        'If a sensor borders 1 or 3 tunnels, it is activated.<br>If a sensor borders 0, 2 or 4 tunnels, it is deactivated.<br><img src=\'static/tutorial_3.png\' style=\'width:80%;margin-top:12px;\'>',
+                        'In the game, you cannot see the tunnels from the surface.<br><img src=\'static/tutorial_4.png\' style=\'width:80%;margin-top:12px;\'>',
+                        'Mark fields for correction (dig or fill tunnels) until all sensors are off.<br><img src=\'static/tutorial_5.png\' style=\'width:80%;margin-top:12px;\'>',
+                        'Mark fields for correction (dig or fill tunnels) until all sensors are off.<br><img src=\'static/tutorial_6.png\' style=\'width:80%;margin-top:12px;\'>',
+                        'Until all sensors are off.<br><img src=\'static/tutorial_7.png\' style=\'width:80%;margin-top:12px;\'>',
+                        'There are several ways to reach the goal! Take the shortest one!<br><br>A loop of tunnels like this is not a problem.<br><img src=\'static/tutorial_8.png\' style=\'width:80%;margin-top:12px;\'>',
+                        'Only if the beavers can get from one side to the other at the end, you lose - a logical error!<br><b>Logical error:</b> A continuous tunnel that is not detected by the sensors and lets the beavers get to the other side.<br><img src=\'static/tutorial_9.png\' style=\'width:80%;margin-top:12px;\'>',
+                        'In playground mode, you can set errors manually and directly observe the effects.<br><img src=\'static/tutorial_10.png\' style=\'width:80%;margin-top:12px;\'>',
+                        'Try to eliminate all errors without causing a logical error. Progress through the levels for more challenge!<br>Good luck!'
+                        ]
     }
   };
 
